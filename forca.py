7f0e73ed67e3ff9391ad6aec10f98a570e158111
alan04 <alandocarmoabilio@gmail.com>
2023-05-16 19:41:14 -0300

    ramdomização de palavras

--- forca.py
+++ forca.py
@@ -1,8 +1,47 @@
 import jogos
 def  jogar():
-    print("**************************")
-    print("Bem vindo ao jogo de Forca")
-    print("**************************")
+    import random
+    print("********************************")
+    print("***Bem vindo ao jogo de Forca***")
+    print("********************************")
+
+    arquivo = open("palavras.txt", 'r')
+    palavras = []
+    for linha in arquivo:
+        palavras.append(linha.strip())
+
+    arquivo.close()
+    sorteada = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[sorteada].upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    enforcou = False
+    acertou = False
+    tentativas = 0
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+
+        chute = input("Escolha uma letra: ")
+        chute = chute.strip().upper()
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            tentativas += 1
+            print("Você errou. Você ainda tem {} tentativas.".format(6-tentativas))
+
+        enforcou = tentativas == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você acertou! Parabéns, palavra era {}!".format(palavra_secreta))
+    else:
+        print("Você perdeeu :(")
     print("Fim de jogo")
     quit()
 
